8a43c547263117b757371bbb6cb8cf5895f46a1a
Joann Feng <joannfeng@wireless-10-148-27-236.public.utexas.edu>
2021-04-20 11:26:40 -0500

    thanks for the help ben

--- matrix-multiply.js
+++ matrix-multiply.js
@@ -40,7 +40,7 @@ function isValid(m1col, m2row, id) {
 }
 
 // next time, try to display inputs in the shape of the matrix
-function tableCreate(rows, columns) {
+function tableCreate(rows, columns, matrixNumber) {
     var body = document.body,
         tbl  = document.createElement('table');
         tbl.style.width  = '100px';
@@ -53,7 +53,7 @@ function tableCreate(rows, columns) {
             var td = tr.insertCell();
             var input = document.createElement("input");
             input.type = "number";
-            input.id = "matrix [" + i + "," + j + "]";
+            input.id = "m" + matrixNumber + "[" + i + "," + j + "]";
             td.appendChild(input);
         }
     }
@@ -124,8 +124,7 @@ function getRowCol(){
 /* instead of instantly adding a table to the body, let this function create an instance of a table to use later :)
     that way, you can easily append it to the form rather than the body.
 */
-function tableCreate_S(rows, columns) {
-    
+function tableCreate_S(rows, columns, matNum) {
     tbl  = document.createElement('table');
     tbl.style.width  = '100px';
     tbl.style.border = '1px solid black';
@@ -137,11 +136,10 @@ function tableCreate_S(rows, columns) {
             var td = tr.insertCell();
             var input = document.createElement("input");
             input.type = "number";
-            input.id = "matrix [" + i + "," + j + "]";
+            input.id = "matrix" + matNum + "[" + i + "," + j + "]";
             td.appendChild(input);
         }
     }
-
     return tbl
 }
 /*
@@ -153,13 +151,14 @@ function createForm_S() {
     form = document.createElement('form') // create a form <form> ... </form>
     form.id = "two_matrix";
     var dim = getRowCol(); // I'm getting the dimensions of the two matrices
-    var mat1 = tableCreate_S(dim.row1, dim.col1); // Creating <table>...</table> elements for both matrices
-    var mat2 = tableCreate_S(dim.row2, dim.col2);
+    var mat1 = tableCreate_S(dim.row1, dim.col1, 1); // Creating <table>...</table> elements for both matrices
+    var mat2 = tableCreate_S(dim.row2, dim.col2, 2);
     
     var submit = document.createElement('button'); // these lines create the submit button
     submit.innerHTML = "Multiply";
     submit.type = "button"; // THIS IS SUPER IMPORTANT <-- buttons inside forms automatically refresh on submit, so you need to make sure the type is button so it doesn't
     submit.onclick = getFormOutput_S; // the reason why I don't put getFormOutput_S() is because the parentheses tells the program to actually run the function when I just need to pass the function itself.
+    //submit.onclick = matrix_multiply(mat1, mat2);
 
     form.appendChild(mat1); // appends everything. Think of it kind of like a tree ...
     form.appendChild(mat2);
@@ -178,5 +177,37 @@ function getFormOutput_S(){
     for (let input of inputs){  // should print out to console the IDs and Values(ctrl-shift-I)
         console.log(input.id);
         console.log(input.value);
+    }
+    matrix_multiply(inputs);
+}
+
+/**
+ * multiply mat1 and mat2 using ijk loops
+ * store result in result
+ **/
+function matrix_multiply(inputs)
+{
+    var sus = getRowCol();
+    var i = 0;
+    var j = 0;
+    var k = 0;
+    for (i; i < sus.row1; i++) {
+        for (j; j < sus.col2; j++) {
+            for (k; k < sus.row2; k++) {
+                //var results = +document.getElementById("addTweets").value;
+                //result = mat1[i][k] * mat2[k][j]; /*id = "matrix" + matNum + "[" + i + "," + j + "]"*/
+                var mat1id = "matrix" + 1 + "[" + i + "," + k + "]";
+                console.log("mat 1 id is " + mat1id);
+                var mat2id = "matrix" + 2 + "[" + k + "," + j + "]";
+                console.log("mat 2 id is " + mat2id);
+                var elem1 = +document.getElementById(mat1id).value;
+                console.log("elem1 is " + elem1);
+                var elem2 = +document.getElementById(mat2id).value;
+                console.log("elem2 is " + elem2);
+                var result = elem1 * elem2;
+                console.log("result is " + result);
+                document.getElementById("result").innerHTML = "" + result;
+            }    
+        }
     }
 }
